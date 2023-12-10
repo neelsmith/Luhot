a41167ab10fef0f49c4574bdb6398962fb542f5d
Neel Smith <dnsmith.neel@gmail.com>
2023-12-09 19:30:05 -0500

    start on qal imperfect

--- src/generate/verbs/qal.jl
+++ src/generate/verbs/qal.jl
@@ -36,8 +36,6 @@ function qal_perfect(root::String,form::HebrewFiniteVerb)
 
 end
 
-# Woo hoo, singular complete!
-
 """Generate requested person+number+gender of the perfect for the given strong verb."""
 function qal_perfect_strong(root::String,verb::HebrewFiniteVerb)
     consonants = collect(BiblicalHebrew.unpointed(root))
@@ -75,6 +73,66 @@ function qal_perfect_strong(root::String,verb::HebrewFiniteVerb)
         string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.ole(BiblicalHebrew.patah(consonants[2])), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.mappiq("נו"))
 
 
+    else
+        @warn("Form $(form) not recognized or not yet implemented.")
+    end
+    
+end
+
+
+
+
+function qal_imperfect(root::String,form::HebrewFiniteVerb)
+    #verbclass = rootclass(root)    
+
+    #if verbclass == :strong
+        qal_imperfect_strong(root, form)
+    #else
+    #    @warn("Verb class $(verbclass) not yet implemented")
+    #    nothing
+    #end
+
+end
+
+
+"""Generate requested person+number+gender of the perfect for the given strong verb."""
+function qal_imperfect_strong(root::String,verb::HebrewFiniteVerb)
+    consonants = collect(BiblicalHebrew.unpointed(root))
+    form = pngSummary(verb)
+    
+    if form == "3sm"
+        string(BiblicalHebrew.hiriq("י"), BiblicalHebrew.sheva(consonants[1]), BiblicalHebrew.holam(consonants[2]), consonants[3])
+
+        #=
+    elseif form == "3sf"
+        string(BiblicalHebrew.qamats(consonants[1]) |> BiblicalHebrew.metheg, BiblicalHebrew.sheva(consonants[2]), BiblicalHebrew.qamats(consonants[3]), BiblicalHebrew.he_ch)
+
+    elseif form == "2sm"
+        string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.patah(BiblicalHebrew.ole(consonants[2])), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.dagesh("ת") |> BiblicalHebrew.qamats)
+
+    elseif form == "2sf"
+        string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.patah(consonants[2]), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.dagesh("ת") |> BiblicalHebrew.sheva)
+    
+
+    elseif form == "1sc" || form == "1sm" || form == "1sf"
+        string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.patah(BiblicalHebrew.ole(consonants[2])), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.mappiq("נו"))
+
+
+    elseif form == "3pc"
+        string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.sheva(consonants[2]), consonants[3], BiblicalHebrew.madppiq("נו"))
+
+
+    elseif form == "2pm"
+        string(BiblicalHebrew.sheva(consonants[1]), BiblicalHebrew.patah(consonants[2]), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.seghol(BiblicalHebrew.dagesh("ת")), "מ")
+
+    elseif form == "2pf"
+        string(BiblicalHebrew.sheva(consonants[1]), BiblicalHebrew.patah(consonants[2]), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.seghol(BiblicalHebrew.dagesh("ת")), "ן")
+
+
+    elseif form == "1pc"
+        string(BiblicalHebrew.qamats(consonants[1]), BiblicalHebrew.ole(BiblicalHebrew.patah(consonants[2])), BiblicalHebrew.sheva(consonants[3]), BiblicalHebrew.mappiq("נו"))
+
+=#
     else
         @warn("Form $(form) not recognized or not yet implemented.")
     end
--- src/generate/verbs/verbs.jl
+++ src/generate/verbs/verbs.jl
@@ -6,8 +6,14 @@ function generate(stem::VerbStem, verb::HebrewFiniteVerb)::String
     triliteral = root(stem)
     if label(hmpPattern(verb)) == "qal" && label(hmpTense(verb)) == "perfect"
         qal_perfect(triliteral, verb)
+
+    elseif label(hmpPattern(verb)) == "qal" && label(hmpTense(verb)) == "imperfect"
+        qal_imperfect(triliteral, verb)
+
+
     else
-        @warn("$(verbhead) + $(tense) not recognized or not yet implemented")
+        @warn("")
+        #@warn("$(verbhead) + $(tense) not recognized or not yet implemented")
         ""
     end
 end
