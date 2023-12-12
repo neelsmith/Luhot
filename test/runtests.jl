2adf61026fd8253ab0e4acac91f57a085b2f25ed
Neel Smith <dnsmith.neel@gmail.com>
2023-12-12 14:07:19 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,9 @@ using Test
 
 
 include("test_dataset.jl")
+include("test_generate.jl")
 include("test_properties.jl")
+include("test_rootclassid.jl")
+include("test_verbs.jl")
 
- include("stems/test_verbstems.jl")
+include("stem_tests/test_verbstems.jl")
---
+++ test/test_rootclassid.jl
@@ -0,0 +1,21 @@
+@testset "Test identifying root class of a triliteral verb" begin
+    strong = "קָטַל"
+    begadkefat = "כָתַב"
+
+    peguttural = "עָבַד"
+    pealeph = "אָחַר"
+    pewawyod = "יָלַד"
+
+    ayinguttural = "בָּחַר"
+    ayinwawyod  = "קָם"
+
+    lamedguttural = "שָׁמַע"
+    lamedaleph = "בָּרָא"
+    lamedhe = "בָּנָה"
+   
+    hollow = "סָבַב"
+
+
+
+    
+end
