254445d153169c69a1207ca1a62b39912baf41d4
Neel Smith <dnsmith.neel@gmail.com>
2023-12-12 03:41:43 -0500

    Add hiphil perfect

--- src/generate/verbs/hiphil.jl
+++ src/generate/verbs/hiphil.jl
@@ -62,25 +62,25 @@ function hiphil_perfect_strong(root::String,verb::HebrewFiniteVerb)
     # for consonants 2-3, check depending on person-number for need of dagesh
 
     if form == "3sm"
-        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(consonants[1]), BiblicalHebrew.hiriq(consonants[2]), "י", consonants[3])
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.hiriq(ayin), "י", lamed)
     elseif form == "3sf"
-        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(consonants[1]), BiblicalHebrew.hiriq(consonants[2]), "י", consonants[3])
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.ole(BiblicalHebrew.hiriq(ayin)), "י", BiblicalHebrew.qamats(lamed),"ה")
     elseif form == "2sm"
-        "..."
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.ole(BiblicalHebrew.patah(ayin)),  BiblicalHebrew.sheva(lamed), BiblicalHebrew.dagesh(BiblicalHebrew.qamats("ת")))
     elseif form == "2sf"
-        "..."        
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.patah(ayin),  BiblicalHebrew.sheva(lamed), BiblicalHebrew.dagesh(BiblicalHebrew.sheva("ת")))        
     elseif form == "1sc" || form == "1sm" || form == "1sf"
-        "..."    
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.ole(BiblicalHebrew.patah(ayin)),  BiblicalHebrew.sheva(lamed), BiblicalHebrew.dagesh(BiblicalHebrew.hiriq("ת")),"י")    
        
         
     elseif form == "3pc" || form == "3pm" || form == "3pf"
-        "..."        
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.ole(BiblicalHebrew.hiriq(ayin)), "י", lamed, BiblicalHebrew.mappiq("ו"))
     elseif form == "2pm"
-        "..."    
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.patah(ayin), BiblicalHebrew.sheva(lamed), BiblicalHebrew.dagesh(BiblicalHebrew.seghol("ת")), "ם")    
     elseif form == "2pf"
-        "..."     
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.patah(ayin), BiblicalHebrew.sheva(lamed), BiblicalHebrew.dagesh(BiblicalHebrew.seghol("ת")), "ן")
     elseif form == "1pc" || form == "1pm" || form == "1pf"
-        "..."
+        string(BiblicalHebrew.hiriq("ה"), BiblicalHebrew.sheva(pe), BiblicalHebrew.ole(BiblicalHebrew.patah(ayin)), BiblicalHebrew.sheva(lamed), "נ", BiblicalHebrew.mappiq("ו"))
     else
         @warn("Form $(form) not recognized or not yet implemented.")
     end
